32e0caaf7a834f15cb09ec6de0b64cdd8ede29fd
Lư Trường Thắng <lutruongthang2511@gmail.com>
2023-05-26 12:49:30 +0700

    home/index

--- CuaHangThoiTrang/Controllers/HomeController.cs
+++ CuaHangThoiTrang/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CuaHangThoiTrang.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,16 @@ namespace CuaHangThoiTrang.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
         {
             return View();
         }
---
+++ CuaHangThoiTrang/DAO/DanhMucDAO.cs
@@ -0,0 +1,24 @@
+﻿using CuaHangThoiTrang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CuaHangThoiTrang.DAO
+{
+    public class DanhMucDAO
+    {
+        CHThoiTrangDbContext context;
+        
+        public DanhMucDAO()
+        {
+            context = new CHThoiTrangDbContext();
+        }
+
+        public IQueryable<DANHMUC> ListDanhMuc()
+        {
+            var res = (from ss in context.DANHMUCs select ss);
+            return res;
+        }
+    }
+}
